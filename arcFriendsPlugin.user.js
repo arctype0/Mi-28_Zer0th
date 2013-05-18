047b4697dcea39d7488744155669ae13d4438674
arctype0 <arctype0@gmail.com>
2013-05-18 15:44:31 +0900

    54353

--- arcFriendsPlugin.user.js
+++ arcFriendsPlugin.user.js
@@ -161,7 +161,7 @@ DiceFriendsPlugin = {
 				$('<div>').addClass('comcenter-friend-draggable-dummy'),
 				$('<div>').addClass('comcenter-avatar').append(
 					$('<div>').attr('rel', player.userId).addClass('base-avatar-container base-avatar-size-small').append(
-						$('<i>').addClass('avatar medium playing')
+						$('<i>').addClass('avatar medium playing').append(
 							.attr('style',bakground-image: )
 							.attr('url', 'http://www.gravatar.com/avatar/'+ player.userAvatar +'?s=36&d=http%3A%2F%2Fbattlelog-cdn.battlefield.com%2Fpublic%2Fbase%2Fshared%2Fdefault-avatar-36.png%3Fv%3D7909')
 									  .attr('width', 36)
