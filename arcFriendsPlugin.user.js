0be745765d0aaee02fc06809bfc0a0e5b19e0392
arctype0 <arctype0@gmail.com>
2013-05-31 18:37:15 +0900

    Update arcFriendsPlugin.user.js

--- arcFriendsPlugin.user.js
+++ arcFriendsPlugin.user.js
@@ -163,8 +163,8 @@ DiceFriendsPlugin = {
 					$('<div>').attr('rel', player.userId).addClass('base-avatar-container base-avatar-size-medium').append(
 						$('<i>').addClass('avatar medium playing').append(
 							$('<img>').attr('src', 'http://www.gravatar.com/avatar/'+ player.userAvatar +'?s=36&d=http%3A%2F%2Fbattlelog-cdn.battlefield.com%2Fpublic%2Fbase%2Fshared%2Fdefault-avatar-36.png%3Fv%3D7909')
-									  .attr('width', 28)
-									  .attr('height', 28)
+									  .attr('width', 36)
+									  .attr('height', 33)
 						)
 					)
 				),
